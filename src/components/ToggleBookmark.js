c4cc4b57552806cac8238b656b5e4cad70f66821
Mano <manoaway@gmail.com>
2020-08-21 20:56:22 +0200

    change svg

--- src/components/ToggleBookmark.js
+++ src/components/ToggleBookmark.js
@@ -8,7 +8,7 @@ const ToggleBookmark = () => {
 
 
   return(
-<svg className={`toggleBookmark ${selected ? "toggleBookmark-active" : ""}`} onClick={()=>toggleSelected(!selected)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22 2v17.582l-4-3.512-4 3.512v-17.582h8zm2-2h-12v24l6-5.269 6 5.269v-24zm-14 1h-10v2h10v-2zm0 5h-10v2h10v-2zm0 5h-10v2h10v-2zm0 5h-10v2h10v-2z"/></svg>
+<svg className={`toggleBookmark ${selected ? "toggleBookmark-active" : ""}`} onClick={()=>toggleSelected(!selected)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14.568.075c2.202 1.174 5.938 4.883 7.432 6.881-1.286-.9-4.044-1.657-6.091-1.179.222-1.468-.185-4.534-1.341-5.702zm-.824 7.925s1.522-8-3.335-8h-.409v12l-2.5-2.634-2.5 2.634v-12h-3v24h20v-13c0-3.419-5.247-3.745-8.256-3z"/></svg>
   )
 }
 
--- src/components/ToggleFavorite.js
+++ src/components/ToggleFavorite.js
@@ -5,7 +5,7 @@ const ToggleFavorite = () => {
   const [selected, toggleSelected]=useState(false);
 
   return (
-  <svg className={`toggleFavorite ${selected? "toggleFavorite-active":""}` } onClick={()=>toggleSelected(!selected)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.876l2.298 5.109 5.342.737-3.851 3.7.931 5.575-4.72-2.897-4.72 2.898.931-5.575-3.851-3.7 5.342-.737 2.298-5.11zm0-4.876l-3.668 8.155-8.332 1.151 6.064 5.828-1.48 8.866 7.416-4.554 7.416 4.554-1.48-8.866 6.064-5.828-8.332-1.15-3.668-8.156z"/></svg>
+  <svg className={`toggleFavorite ${selected? "toggleFavorite-active":""}` } onClick={()=>toggleSelected(!selected)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
   )
 }
 
