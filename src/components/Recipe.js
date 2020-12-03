ecf3ef9f02aeb960f8aefd94b991d6a3f17e337d
Zsuzsa Lukacs <manoaway@gmail.com>
2020-12-03 14:29:35 +0100

    put icons on the back

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -9,10 +9,10 @@ const Recipe = ({title, calories, image, ingredients, key}) => {
   <div className ={style.recipe}>
   <div className ={style.header}>
 
-    <ToggleFavorite/>
-   <ToggleBookmark/>
+
    </div>
     <section className ={style.about}>
+
     <div className ={style.recipe__fotos}>
   <div className ={style.flip_box}>
 <div className ={style.flip_box__inner}>
@@ -20,6 +20,10 @@ const Recipe = ({title, calories, image, ingredients, key}) => {
 <img src={image} alt="{title}" className ={style.image} />
 </div>
 <div className ={style.flip_box__back}>
+<div className = {style.icons}>
+<ToggleFavorite/>
+<ToggleBookmark/>
+</div>
     <h2 className ={style.title} >{title}</h2>
     <aside className ={style.nutrition}>
     <p>Calories per serving:{' '+Math.floor(calories)}</p>
