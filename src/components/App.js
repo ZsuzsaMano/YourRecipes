a31f3bfdb8d027f0d00d68075391e04513bf2361
Mano <manoaway@gmail.com>
2020-12-04 21:57:52 +0100

    change api comment out ingredients

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,76 @@
-import React, {useEffect, useState} from 'react';
+import React, {
+  useEffect,
+  useState
+} from 'react';
 import Recipe from './Recipe';
 import Navbar from './Navbar';
+import apiKey from './apiKey';
 import '../styles/App.min.css';
 
 function App() {
-  const APP_ID = '2c15a1a3';
-  const APP_KEY = 'ea62895795633e11ae58be941c08bf0d';
 
-const [recipes, setRecipes] = useState([]);
-const [search, setSearch] = useState('');
-const [query, setQuery] = useState('vegetarian')
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('vegetarian')
 
 
-  useEffect(()=> {
+  useEffect(() => {
     getRecipes();
   }, [query]);
 
-  const getRecipes = async() => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&calories=591-722`);
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&tags=vegetarian&apiKey=${apiKey}`);
     const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
+    console.log(data.recipes);
+    setRecipes(data.recipes);
   };
 
-    const updateSearch= e =>{
-      setSearch(e.target.value)
-    }
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
 
-    const getSearch = e =>{
-      e.preventDefault();
-      setQuery(search);
-      setSearch('');
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+  return ( <
+    div className = "App" >
+    <
+    Navbar getSearch = {
+      getSearch
     }
-  return (
-    <div className="App">
-    <Navbar
-        getSearch = {getSearch}
-        search = {search}
-        updateSearch = {updateSearch}/>
+    search = {
+      search
+    }
+    updateSearch = {
+      updateSearch
+    }
+    />
 
-      <div className="recipes">
-     {recipes.map((recipe,index)=>(
-        <Recipe
-                key={'Recipe' + index}
-                title={recipe.recipe.label}
-                calories= {recipe.recipe.calories}
-                image= {recipe.recipe.image}
-                ingredients={recipe.recipe.ingredients}
-              />
-     ))}
-     </div>
-    </div>
+    <
+    div className = "recipes" > {
+      recipes.map((recipe) => ( <
+        Recipe id = {
+          recipe.id
+        }
+        title = {
+          recipe.title
+        }
+        // calories = {
+        //   recipe.calories
+        // }
+        image = {
+          recipe.image
+        }
+        // ingredients = {
+        //   recipe.ingredients
+        // }
+        />
+      ))
+    } <
+    /div> < /
+    div >
   );
 }
 
