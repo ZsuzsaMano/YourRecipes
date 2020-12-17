f78e6feb7f4936421444a7cdcf7cfa957c100998
Mano <manoaway@gmail.com>
2020-12-17 22:29:19 +0100

    add pages

---
+++ src/pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () =>{
+  return(
+    <div>
+    <h1>Please Login to continue</h1>
+    <form action="">
+    <input type="text"/>
+    <input type="email"/>
+    </form>
+    </div>
+  )
+
+}
+
+export default Login;
