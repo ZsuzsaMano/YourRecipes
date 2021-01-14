da3c56e2d723374b650e46b2a6f752ca25909473
Zsuzsa Lukacs <manoaway@gmail.com>
2021-01-14 21:17:07 +0100

    mark selectedelement on login

--- src/components/ToggleBookmark.jsx
+++ src/components/ToggleBookmark.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { LoginContext } from '../context/LoginContext';
 import { Link } from 'react-router-dom';
 
@@ -25,6 +25,14 @@ const ToggleBookmark = ({ id, title, image }) => {
     }
   };
 
+  useEffect(() => {
+    if (myBookmarkedRecipies.find((inv) => inv.id === id)) {
+      toggleSelected(true);
+    } else {
+      toggleSelected(false);
+    }
+  },[])
+
   if (isLoggedin) {
     return (
       <div className="bookmark">
@@ -41,7 +49,7 @@ const ToggleBookmark = ({ id, title, image }) => {
   viewBox="0 0 475.82 442.01"
   version="1.0">
              <g id="layer1" transform="translate(-134.07 -225.8)">
-               <path id="path7" d="m263.42 235.15c-66.24 0-120 53.76-120 120 0 134.75 135.93 170.08 228.56 303.3 87.57-132.4 228.56-172.85 228.56-303.3 0-66.24-53.76-120-120-120-48.05 0-89.4 28.37-108.56 69.18-19.16-40.81-60.52-69.18-108.56-69.18z" stroke-width="18.7" />
+               <path id="path7" d="m263.42 235.15c-66.24 0-120 53.76-120 120 0 134.75 135.93 170.08 228.56 303.3 87.57-132.4 228.56-172.85 228.56-303.3 0-66.24-53.76-120-120-120-48.05 0-89.4 28.37-108.56 69.18-19.16-40.81-60.52-69.18-108.56-69.18z" />
              </g>
            </svg>
         <p className="loginToBookmark">Bookmark Recipe</p>{' '}
